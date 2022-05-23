50f7ce674131e5d5ae54341259bc76c331f37788
Antoine Pascual <antoinepascualmail@gmail.com>
2022-05-23 12:35:38 +0200

    Lancement du server node

---
+++ back/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const config = require('./config/server');
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const cors = require("cors");
+const PORT = process.env.PORT || 3001;
+var ObjectId = require('mongodb').ObjectId;
+
+
+app.use(cors());
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.set('view engine', 'ejs');
+mongoose.connect(config.DB_URI, function(err, db){
+    if (err) throw err;
+    console.log('Connected to database');
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+     console.log("Node Js Server is Running on port " + PORT);
+ })
