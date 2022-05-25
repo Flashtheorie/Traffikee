b078602d6106246e462903c3a1c37e2b1d5b3afb
Antoine Pascual <antoinepascualmail@gmail.com>
2022-05-25 20:14:53 +0200

    User data into the form account settings

--- front/src/app/profil/profil.component.ts
+++ front/src/app/profil/profil.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.css']
 })
-export class ProfilComponent implements OnInit {
+export class ProfilComponent  {
   getSessionInfos(){
     return sessionStorage.getItem('id'); 
   }
-  constructor() { }
 
-  ngOnInit(): void {
-  }
 
+  url = 'http://localhost:3001/users/' + sessionStorage.getItem('id');
+  user = [];
+
+
+ constructor(private http: HttpClient){
+  this.http.get(this.url).toPromise().then((data: any) => {
+  this.user = data
+  })
+
+ 
+
+}
 }
