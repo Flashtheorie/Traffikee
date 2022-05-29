15b9d26ec27777267e3747db549ccbcb8c87b560
Antoine Pascual <antoinepascualmail@gmail.com>
2022-05-29 12:39:44 +0200

    Pop up delete your account

--- front/src/app/profil/profil.component.ts
+++ front/src/app/profil/profil.component.ts
@@ -1,12 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { trigger, transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
-  styleUrls: ['./profil.component.css']
+  styleUrls: ['./profil.component.css'],
+  animations: [ 
+    trigger('opacity', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-out', style({  opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('200ms ease-in', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('opacityTranslateY', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(1rem)' }),
+        animate('300ms ease-out', style({  opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translateY(0)' }),
+        animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(1rem)' }))
+      ])
+    ])
+  ]
 })
 export class ProfilComponent  {
+
+  isModal = false;
+
+  
+  
+  toggleModal(){
+    this.isModal = !this.isModal;
+  }
+
+
   getSessionInfos(){
     return sessionStorage.getItem('id'); 
   }
