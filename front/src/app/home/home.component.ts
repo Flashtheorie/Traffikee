b5429477f9dcd85a08209cc8da3cd7cf16834840
Antoine Pascual <antoinepascualmail@gmail.com>
2022-06-01 13:54:41 +0200

    FAQ in objects

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -6,6 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  faq = [
+    {
+      question: 'What is Traffikee ?',
+      answer: 'We are Venom'
+    }, 
+    {
+      question: 'What does Trafikee do ?',
+      answer: 'We poo.'
+    },
+    {
+      question: 'What',
+      answer: ''
+    }
+  ]
   getSessionInfos(){
     return sessionStorage.getItem('id'); 
   }
